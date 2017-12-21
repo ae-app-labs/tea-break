c670579651aceda0cb46b3d29599e2d9e64b5ca7
Midhun Harikumar <midhunhk@gmail.com>
2017-12-21 12:24:06 +0530

    use firebase users doc

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 
 (function(){
     // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCvEAF-ZBP6EXM1Z51k0ZCpMBj4J0xxPKg",
-    authDomain: "tea-break.firebaseapp.com",
-    databaseURL: "https://tea-break.firebaseio.com",
-    projectId: "tea-break",
-    storageBucket: "tea-break.appspot.com",
-    messagingSenderId: "972097047700"
-  };
-  firebase.initializeApp(config);
+	var config = {
+		apiKey: "AIzaSyCvEAF-ZBP6EXM1Z51k0ZCpMBj4J0xxPKg",
+		authDomain: "tea-break.firebaseapp.com",
+		databaseURL: "https://tea-break.firebaseio.com",
+		projectId: "tea-break",
+		storageBucket: "tea-break.appspot.com",
+		messagingSenderId: "972097047700"
+	};
+	firebase.initializeApp(config);
     
+	// Get reference to firebase database
+	const usersRef = firebase.database().ref("users");
+	
+	// Add listener to child_added event
+	usersRef.on("child_added", function(snapshot){
+		console.log(snapshot.val());
+		showUsersAndCount(snapshot.val());
+	});
+	
+	function showUsersAndCount(data){
+		$("#list").append("<li>" + data.name + " = " + data.count + "</li>");
+	}
+	
+	/*
     var firestore = firebase.firestore();
     const docRef = firestore.doc("boost/dec");
     
@@ -18,7 +32,7 @@
         .then(function (doc){
             if(doc && doc.exists){
                 const mydata = doc.data();
-                showData(mydata);
+                // showData(mydata);
             }
         }).catch(function(error){
             console.log("error " , error);
@@ -33,5 +47,6 @@
                 .appendTo("#list")
         }
     }
+	*/
     
 })();
