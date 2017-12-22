2269715f6247636c4ba21b0431dab82f8035c8f5
Midhun Harikumar <midhunhk@gmail.com>
2017-12-22 17:59:08 +0530

    more variants for list handling

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,20 +5,38 @@ const React = require('react')
 class Table extends React.Component{
     constructor(props){
         super(props);
+        this.addUsers = this.addUsers.bind(this)
         this.state = {
             usersData : []
         }
     }
-    
+
+    addUsers(users){
+      this.setState({usersData: users });
+    }
+
     componentDidMount(){
       TeaBreak.init();
-      
+
       TeaBreak.userValues( data => {
           // console.log( JSON.stringify(data, null, 3) );
           const users = [];
-          
-          console.log(data.val());
-          
+          //console.log(data.val());
+
+          // Variant 2
+          const keys = Object.keys(data.val())
+          console.log(keys)
+          for(userSnap in keys){
+            var user = {
+              id : userSnap.key,
+              name : userSnap.val().name,
+              count : userSnap.val().count
+            }
+            users.push(user);
+          }
+
+          // Variant 1
+          /*
           data.forEach( function(snap){
               // console.log(snap.key);
               users.push({
@@ -27,27 +45,30 @@ class Table extends React.Component{
                   count : snap.val().count
               });
           });
-          
-          this.setState({
-              usersData: users
-          });
-          // console.log("End userValues callback" + this.state.usersData);
+          */
+
+          // Variant 3
+          /*
+            data.forEach(function(childSnapshot) {
+              var item = childSnapshot.val();
+              item['.key'] = childSnapshot.key;
+              users.push(item);
+            }.bind(this));
+          */
+
+          addUsers(users);
       });
     }
-    
+
     render(){
-        
-        const listOfUsers = this.state.usersData.map(position => {
-            console.log(position.name);
+        const listOfUsers = this.state.usersData.map(position =>
             <tr>
                 <td className="mdl-data-table__cell--non-numeric">{position.name}</td>
                 <td>{position.count}</td>
                 <td>{position.id}</td>
             </tr>
-        });
-        
-        // console.log("in render " + this.state.usersData);
-        
+        );
+
         return(
           <table className="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
             <thead>
